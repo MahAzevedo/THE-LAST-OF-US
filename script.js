1d903810d75bd8dd79f087e37ce536ff175f67fd
Mah <madavetto@gmail.com>
2023-03-16 15:59:05 -0300

    updates

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-const episodes = [ //dado mocado: onde passam a descricao do objeto 
+ //dado mocado: onde passam a descricao do objeto 
+const episodes = [
     {
-        img: "./img/The_Last_of_Us_-_When_You're_Lost_in_the_Darkness 3.png",
+        img: "img/tlou_ep.1_s1.png",
         title: "When You're Lost In The Darkness",
         description:
-            "In 2003, a mass fungal infection of mutated Cordyceps 
-            sparks a global pandemic. Joel flees with his daughter, 
-            Sarah, and brother, Tommy, from their Texas home; 
-            Sarah is killed by a soldier.",
+            "In 2003, a mass fungal infection of mutated Cordyceps sparks a global pandemic. Joel flees with his daughter, Sarah, and brother, Tommy, from their Texas home; Sarah is killed by a soldier.",
     },
 
     {
-        img: "./img/The_Last_of_Us_-_Infected 4.png",
+        img: "img/tlou_ep2_s1_Infected.png",
         title: "Infected",
         description:
-            "Two days before the worldwide outbreak, in Jakarta, 
-            Indonesia, the government shows an infected corpse to 
-            a mycologist, who tells them there is no cure or vaccine.",
+            "Two days before the worldwide outbreak, in Jakarta, Indonesia, the government shows an infected corpse to a mycologist, who tells them there is no cure or vaccine.",
     },
 
     {
-        img: "./img/The_Last_of_Us_-_Please_Hold_to_My_Hand 2.png",
+        img: "img/tlou_s1.ep3.png",
         title: "Long, Long Time",
         description:
-            "Joel and Ellie begin the hike to meet with Bill and Frank. 
-            Ellie sees evidence of the government's execution of 
-            innocents during the early days of the pandemic.",
+            "Joel and Ellie begin the hike to meet with Bill and Frank. Ellie sees evidence of the government's execution of innocents during the early days of the pandemic.",
     },
 
     {
-        img: "img/The_Last_of_Us_-_Long_Long_Time 2.png",
+        img: "img/tlou_s1_ep4.png",
         title: "Please Hold to My Hand",
         description:
-            "Traveling through Missouri, Joel and 
-            Ellie are forced to take a detour through 
-            Kansas City, where they are ambushed.",
+            "Traveling through Missouri, Joel and Ellie are forced to take a detour through Kansas City, where they are ambushed.",
     },
 
 ];
